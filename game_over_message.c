a98d780727fda66cb38fa9289a770918bb5372dc
Justine Fricou <jfricou@justine.fricou>
2019-12-01 23:19:37 +0100

    [game_over_message.c] Added buttons to play again or quit in game over message

--- game_over_message.c
+++ game_over_message.c
@@ -8,22 +8,26 @@
 #include "include/weapon.h"
 #include "include/play.h"
 
-void set_1st_line(sfRenderWindow *window, sfText *first_line, sfText *score);
+void set_text(sfRenderWindow *w, sfText *line1, sfText *score, sfText *line3);
 void handle_events_game_over(sfRenderWindow *window, sfEvent event);
+void mouse_click_game_over(sfRenderWindow *window, sfMouseButtonEvent event);
 
 void display_game_over(sfRenderWindow *window, sfText *score)
 {
     sfEvent event;
-    sfText *first_line;
-    sfVector2f position1 = {250, 200};
-    sfVector2f position2 = {350, 300};
+    sfText *first_line, *third_line;
+    sfVector2f position1 = {250, 150}, position2 = {350, 220};
+    sfVector2f position3 = {250, 350};
 
     first_line = sfText_create();
+    third_line = sfText_create();
     sfText_setPosition(first_line, position1);
     sfText_setPosition(score, position2);
-    set_1st_line(window, first_line, score);
+    sfText_setPosition(third_line, position3);
+    set_text(window, first_line, score, third_line);
     sfRenderWindow_drawText(window, first_line, NULL);
     sfRenderWindow_drawText(window, score, NULL);
+    sfRenderWindow_drawText(window, third_line, NULL);
     sfRenderWindow_display(window);
     while (sfRenderWindow_isOpen(window)) {
         sfRenderWindow_waitEvent(window, &event);
@@ -31,18 +35,23 @@ void display_game_over(sfRenderWindow *window, sfText *score)
     }
 }
 
-void set_1st_line(sfRenderWindow *window, sfText *first_line, sfText *score)
+void set_text(sfRenderWindow *w, sfText *line1, sfText *score, sfText *line3)
 {
     sfFont *font = NULL;
 
     font = sfText_getFont(score);
-    sfRenderWindow_clear(window, sfWhite);
-    sfText_setString(first_line, "Game over !  Your score :");
-    sfText_setColor(first_line, sfBlack);
+    sfRenderWindow_clear(w, sfWhite);
+    sfText_setString(line1, "Game over !  Your score :");
+    sfText_setString(line3, "Play again                Quit");
+    sfText_setColor(line1, sfBlack);
     sfText_setColor(score, sfBlack);
-    sfText_setCharacterSize(first_line, 50);
-    sfText_setFont(first_line, font);
-    sfText_setStyle(first_line, sfTextBold);
+    sfText_setColor(line3, sfBlack);
+    sfText_setCharacterSize(line1, 50);
+    sfText_setCharacterSize(line3, 50);
+    sfText_setFont(line1, font);
+    sfText_setFont(line3, font);
+    sfText_setStyle(line1, sfTextBold);
+    sfText_setStyle(line3, sfTextBold);
 }
 
 void handle_events_game_over(sfRenderWindow *window, sfEvent event)
@@ -51,9 +60,19 @@ void handle_events_game_over(sfRenderWindow *window, sfEvent event)
     case sfEvtClosed :
         sfRenderWindow_close(window);
         break;
+    case sfEvtMouseButtonReleased :
+        mouse_click_game_over(window, event.mouseButton);
     default :
         break;
     }
+}
+
+void mouse_click_game_over(sfRenderWindow *window, sfMouseButtonEvent event)
+{
+    if (250 <= event.x && event.x <= 400 && 350 <= event.y && event.y <= 400)
+        play(window);
+    if (500 <= event.x && event.x <= 600 && 350 <= event.y && event.y <= 400)
+        sfRenderWindow_close(window);
 }
 
 /*void set_3rd_line(sfRenderWindow *window, sfText *3rd_line, sfText score)
